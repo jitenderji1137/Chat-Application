bfe9f3f9a1b6cfb47ac18974bff68db1eac79e6b
jitender <trademetrader1137@gmail.com>
2023-05-01 07:21:16 +0530

    Free Chat

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ChatBox from "./components/ChatBox";
+import Welcome from "./components/Welcome";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <NavBar />
+      {!user ? (
+        <Welcome />
+      ) : (
+        <>
+          <ChatBox />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBox.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef, useState } from "react";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+const ChatBox = () => {
+    const scroll = useRef();
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        const q = query(
+          collection(db, "messages"),
+          orderBy("createdAt"),
+          limit(150)
+        );
+        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+          let messages = [];
+          QuerySnapshot.forEach((doc) => {
+            messages.push({ ...doc.data(), id: doc.id });
+          });
+          setMessages(messages);
+          scroll.current.scrollIntoView({ behavior: "smooth" });
+        });
+        return () => unsubscribe;
+      }, []);
+  return (
+    <main className="chat-box">
+        <span ref={scroll}></span>
+      <div className="messages-wrapper">
+      {messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      <SendMessage scroll={scroll} />
+    </main>
+  );
+};
+
+export default ChatBox;
